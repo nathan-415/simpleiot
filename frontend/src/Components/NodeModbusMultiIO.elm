dbc6f96ba67e8440fe2134f0456d442bd99082d6
Collin Brake <collinmbrake@gmail.com>
2021-06-07 20:56:29 -0400

    rearrange in start for IO list

--- frontend/src/Components/NodeModbusMultiIO.elm
+++ frontend/src/Components/NodeModbusMultiIO.elm
@@ -54,85 +54,11 @@ view o =
                 , labelWidth = labelWidth
                 }
 
-        optionInput =
-            Form.nodeOptionInput
-                { onEditNodePoint = o.onEditNodePoint
-                , node = o.node
-                , now = o.now
-                , labelWidth = labelWidth
-                }
-
-        checkboxInput =
-            Form.nodeCheckboxInput
-                { onEditNodePoint = o.onEditNodePoint
-                , node = o.node
-                , now = o.now
-                , labelWidth = labelWidth
-                }
-
-        counterWithReset =
-            Form.nodeCounterWithReset
-                { onEditNodePoint = o.onEditNodePoint
-                , node = o.node
-                , now = o.now
-                , labelWidth = labelWidth + 150
-                }
-
-        modbusIOType =
-            Point.getText o.node.points Point.typeModbusIOType
-
-        isClient =
-            case o.parent of
-                Just p ->
-                    Point.getText p.points Point.typeClientServer == Point.valueClient
-
-                Nothing ->
-                    False
-
-        isWrite =
-            modbusIOType
-                == Point.valueModbusHoldingRegister
-                || modbusIOType
-                == Point.valueModbusCoil
-
         value =
             Point.getValue o.node.points Point.typeValue
 
         valueSet =
             Point.getValue o.node.points Point.typeValueSet
-
-        isRegister =
-            modbusIOType
-                == Point.valueModbusInputRegister
-                || modbusIOType
-                == Point.valueModbusHoldingRegister
-
-        isReadOnly =
-            Point.getValue o.node.points Point.typeReadOnly == 1
-
-        valueText =
-            if isRegister then
-                String.fromFloat (Round.roundNum 2 value)
-
-            else if value == 0 then
-                "off"
-
-            else
-                "on"
-
-        valueBackgroundColor =
-            if valueText == "on" then
-                Style.colors.blue
-
-            else
-                Style.colors.none
-
-        valueTextColor =
-            if valueText == "on" then
-                Style.colors.white
-
-            else
-                Style.colors.black
     in
     column
         [ width fill
@@ -142,30 +68,20 @@ view o =
         ]
     <|
         wrappedRow [ spacing 10 ]
-            [ Icon.io
-            , text <|
+            [ text <|
                 Point.getText o.node.points Point.typeDescription
-                    ++ ": "
-            , el [ paddingXY 7 0, Background.color valueBackgroundColor, Font.color valueTextColor ] <|
-                text <|
-                    valueText
-                        ++ (if isRegister then
-                                " " ++ Point.getText o.node.points Point.typeUnits
-
-                            else
-                                ""
-                           )
-            , text <|
-                if isClient && isWrite && not isReadOnly && value /= valueSet then
-                    " (cmd pending)"
-
-                else
-                    ""
             ]
             :: (if o.expDetail then
                     [ textInput Point.typeDescription "Description"
                     , numberInput Point.typeID "ID"
-                    , onOffInput Point.typeValue Point.typeValueSet "DI_01"
+                    , wrappedRow [ spacing 10 ]
+                        [ onOffInput Point.typeValue Point.typeValueSet "DI_01"
+                        , textInput Point.typeDescription ""
+                        ]
+                    , wrappedRow [ spacing 20 ]
+                        [ onOffInput Point.typeValue Point.typeValueSet "DI_02"
+                        , textInput Point.typeDescription ""
+                        ]
                     ]
 
                 else
